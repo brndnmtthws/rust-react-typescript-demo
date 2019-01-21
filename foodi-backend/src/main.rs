048f80b4ef19d0759da70a32462225632af59aa4
Brenden Matthews <brenden@diddyinc.com>
2019-01-21 13:49:05 -0500

    Add post (or add) endpoint.

--- foodi-backend/src/main.rs
+++ foodi-backend/src/main.rs
@@ -8,11 +8,13 @@ extern crate rocket_contrib;
 extern crate diesel;
 #[macro_use]
 extern crate serde_derive;
-
 use rocket::response::content;
+use rocket_contrib::json::{Json, JsonValue};
 
 mod models;
 mod schema;
+
+use models::*;
 
 #[derive(Debug, Responder)]
 #[response(status = 500, content_type = "json")]
@@ -23,24 +25,69 @@ pub struct ResponseError {
 #[database("sqlite")]
 struct DbConn(diesel::SqliteConnection);
 
+#[get("/meals/<meal_id>")] // GET /v1/meals
+fn get(conn: DbConn, meal_id: i32) -> Result<content::Json<String>, ResponseError> {
+    use diesel::prelude::*;
+    use schema::meals::dsl::*;
+    let query_result = meals.filter(id.eq(meal_id)).load::<Meal>(&*conn);
+
+    match query_result {
+        Ok(results) => Ok(content::Json(json!(results).to_string())),
+        Err(error) => Err(ResponseError {
+            response: content::Json(json!({ "error": error.to_string() }).to_string()),
+        }),
+    }
+}
 #[get("/meals")] // GET /v1/meals
 fn list(conn: DbConn) -> Result<content::Json<String>, ResponseError> {
     use diesel::prelude::*;
-    use models::*;
     use schema::meals::dsl::*;
 
-    let results = meals.load::<Meal>(&*conn).expect("Couldn't get meals");
-    Ok(content::Json(json!(results).to_string()))
+    let query_result = meals.load::<Meal>(&*conn);
+
+    match query_result {
+        Ok(results) => Ok(content::Json(json!(results).to_string())),
+        Err(error) => Err(ResponseError {
+            response: content::Json(json!({ "error": error.to_string() }).to_string()),
+        }),
+    }
 }
 
-#[put("/meals")] // PUT /v1/meals
-fn add() -> &'static str {
-    "Hello, world!"
+#[post("/meals", data = "<new_meal>")] // POST /v1/meals
+fn add(conn: DbConn, new_meal: Json<NewMeal>) -> Result<content::Json<String>, ResponseError> {
+    use diesel::prelude::*;
+    use diesel::result::Error;
+
+    let inserted_meal = conn.transaction::<Meal, Error, _>(|| {
+        diesel::insert_into(schema::meals::table)
+            .values(&new_meal.0)
+            .execute(&*conn)?;
+
+        use schema::meals::dsl::*;
+
+        meals.order(id.desc()).first(&*conn)
+    });
+
+    match inserted_meal {
+        Ok(meal) => Ok(content::Json(json!(meal).to_string())),
+        Err(error) => Err(ResponseError {
+            response: content::Json(json!({ "error": error.to_string() }).to_string()),
+        }),
+    }
+}
+
+#[catch(404)]
+fn not_found() -> JsonValue {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
 }
 
 fn main() {
     rocket::ignite()
         .attach(DbConn::fairing())
-        .mount("/v1", routes![list, add])
+        .register(catchers![not_found])
+        .mount("/v1", routes![list, add, get])
         .launch();
 }
--- foodi-backend/src/models.rs
+++ foodi-backend/src/models.rs
@@ -1,10 +1,17 @@
-use chrono;
-use chrono::prelude::*;
+use crate::schema::meals;
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 
 #[derive(Queryable, Serialize)]
 pub struct Meal {
     pub id: i32,
+    pub name: String,
+    pub time: NaiveDateTime,
+}
+
+#[derive(Insertable, Deserialize)]
+#[table_name = "meals"]
+pub struct NewMeal {
     pub name: String,
     pub time: NaiveDateTime,
 }
